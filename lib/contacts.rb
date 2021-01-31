41dbad6990628071a27243f54ba393ac1dbdb85c
Keginold Philip <keginoldp@hotmail.com>
2021-01-31 15:15:38 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
 	contacts.each do |person, contact_details_hash|
 		if person == "Freddy Mercury"
-			contact_details_hash.each do |attribure, data|
+			contact_details_hash.each do |attribute, data|
 				binding.pry
 end
 end
